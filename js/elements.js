7b129a60806348c582ae4e03b66d3544a8b948dc
Generson Silva <generson.avelinosilva@gmail.com>
2022-08-07 13:36:49 -0300

    correction

--- js/elements.js
+++ js/elements.js
@@ -1,15 +1,9 @@
-<<<<<<< Updated upstream
 function abreConfig() {
     const modal = document.querySelector('#modal-container-config')
-=======
-function abreModal() {
-    const modal = document.querySelector('#modal-container')
->>>>>>> Stashed changes
     modal.classList.add('mostrar')
 
 
     modal.addEventListener('click', (e) => {
-<<<<<<< Updated upstream
         if (e.target.id === 'modal-alt-btn' || e.target.id === 'modal-container-config') {
             modal.classList.remove('mostrar')
         }
@@ -33,10 +27,11 @@ function abreInfo() {
 
     modal.addEventListener('click', (e) => {
         if (e.target.id === 'modal-btn-fechar-info' || e.target.id === 'modal-container-info') {
-=======
-        if (e.target.id === 'modal-close-btn' || e.target.id === 'modal-container') {
->>>>>>> Stashed changes
             modal.classList.remove('mostrar')
         }
     })
+}
+
+function calc() {
+
 }
